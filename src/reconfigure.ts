d30c5b7237e3b072c1f0aa86e68fb7d696ffbb1f
Attila Bagossy <bagossyattila@outlook.com>
2021-08-25 19:57:00 +0200

    style: linting fixes throughout

--- src/reconfigure.ts
+++ src/reconfigure.ts
@@ -1,11 +1,11 @@
 import {ConfigurationOptions, optionsKey} from './decorator'
 
 export function reconfigure(
-	reconfigurator: (options: ConfigurationOptions) => void,
-	constructor: new () => any,
+  reconfigurator: (options: ConfigurationOptions) => void,
+  constructor: new () => any,
 ) {
-	// eslint-disable-next-line
+  // eslint-disable-next-line
 	const originalOptions = constructor.prototype[optionsKey]
 
-	reconfigurator(originalOptions)
+  reconfigurator(originalOptions)
 }
